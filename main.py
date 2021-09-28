0ddbfd4537e1d70b39c86dc1097720cbd62f21a1
Scott Economu <scottomo@gmail.com>
2021-09-28 07:55:47 -0700

    Final

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import utils
 
+# ------- LOAD THE FILES AND CHOOSE DATA -----------------
 test_file = utils.read_data("fitbit_data_3-8_9-16.csv")
 test_file2 = utils.read_data("Lake Arthur Data.csv")
 print("")
 #utils.display_table(test_file[0], test_file[1])
-#utils.display_table(test_file2[0], test_file2[1])
+utils.display_table(test_file2[0], test_file2[1])
 print("What data would you like to look at?")
 data_name = input()
 #data_name = 'Steps'
-#utils.statistics(data_name, test_file[0], test_file[1])
-#utils.statistics(data_name, test_file2[0], test_file2[1])
 
-
-#**************************** USING MULTIPLE FUNCTIONS FOR STATISTICS ****************************************
-column = utils.get_column(data_name, test_file[0], test_file[1])
-#column = utils.get_column(data_name, test_file2[0], test_file2[1])
+#--------------------- FUNCTION CALLS --------------------------
+#column = utils.get_column(data_name, test_file[0], test_file[1])
+column = utils.get_column(data_name, test_file2[0], test_file2[1])
 column_data = utils.change_to_numeric(column)
 data_sum = int(utils.statistics_sum(column_data))
 data_average = utils.statistics_average(column_data)
@@ -22,10 +20,11 @@ data_std_dev = utils.statistics_stddev(column_data)
 data_median = int(utils.statistics_median(column_data))
 data_low_num = utils.statistics_low_num(column_data)
 data_high_num = utils.statistics_high_num(column_data)
-low_num_date = test_file[1][data_low_num[1]][0]
-high_num_date = test_file[1][data_high_num[1]][0]
-# low_num_date = test_file2[1][data_low_num[1]][0]
-# high_num_date = test_file2[1][data_high_num[1]][0]
+low_num_date = test_file2[1][data_low_num[1]][0]
+high_num_date = test_file2[1][data_high_num[1]][0]
+
+# --------------------- RESULTS ---------------------------------------
+print("")
 print("There are ", len(column_data), " records for ", data_name, ".", sep='')
 print("The total number of ", data_name, " is ", data_sum, '.', sep='')
 print("The average number of ", data_name, " is ", int(data_average), ".", sep='')
@@ -33,4 +32,3 @@ print("The standard deviation for ", data_name, " is ", int(data_std_dev), ".", 
 print("The median number for ", data_name, " is ", data_median, ".", sep='')
 print("The smallest number for ", data_name, " is ", data_low_num[0], " on ", low_num_date,".", sep='')
 print("The largest number for ", data_name, " is ", data_high_num[0], " on ", high_num_date,".", sep='')
-#**********************************************************************************************************
--- utils.py
+++ utils.py
@@ -47,73 +47,6 @@ def get_column(column_name, headers, data):
             column_data.append(data[i][column_index])  
     return column_data      
 
-
-def statistics(column_name, headers, data):
-    # find the column name index
-    column_index = 0 
-    match = False
-    for name in range(len(headers)):
-        if headers[name] == column_name:
-            column_index = name
-            match = True
-    if match == False:     
-        print("I'm sorry, that column does not exist.")
-          
-    # get the number of elements
-    entries = len(data)
-    print("There are", entries, "records for", headers[column_index], ".")
-
-    # get the sum of the list elements 
-    list_sum = 0
-    num_list = []
-    column_data = get_column(column_name, headers, data) 
-    #date_list = get_column("Date", column_name, data)
-    for value in column_data:
-        num_list.append(float(value))
-    for element in num_list:
-        list_sum = list_sum + element
-    print("The total number of", headers[column_index], "is", list_sum, '.')
- 
-    # calculate the average value of the elements (the mean)
-    list_average = list_sum / entries
-    print("The average number of", headers[column_index], "is", int(list_average), ".")
-    
-    # get the standard deviation
-    list_of_means = []
-    for x in num_list: # subtract mean from element
-        list_of_means.append((x - list_average) ** 2)
-    means_average = 0
-    for x in list_of_means: # get the sum of the means
-        means_average = means_average + x
-    means_average = means_average / len(list_of_means) # get the average of the means
-    std_dev = math.sqrt(means_average)
-    print("The standard deviation for ", headers[column_index], "is ", int(std_dev), ".")
-
-    #sort the list to get the median, largest, and smallest numbers
-    my_list_sorted = num_list[:] 
-    my_list_sorted.sort()
-    median_number = my_list_sorted[int(round((len(my_list_sorted)/2),0))]
-    print("The median number for", headers[column_index], "is", median_number, ".")
-
-    # get the date for the smallest and largest value
-    high_num = 0
-    high_num_index = 0
-    for i in range(len(num_list)):
-        if num_list[i] > high_num:
-            high_num = num_list[i]
-            high_num_index = i
-    low_num = high_num  # ????????????????????????is this not a reference?  high_num doesn't change when I change low_num below. Is that only for lists?????????????????????
-    low_num_index = 0
-    for i in range(len(num_list)):
-        if num_list[i] < low_num:
-            low_num = num_list[i]
-            low_num_index = i
-    print("The smallest number for", headers[column_index], "is", low_num, "on", data[low_num_index][0],".")
-    print("The largest number for", headers[column_index], "is", high_num, "on", data[high_num_index][0],".")
-
-    #return column_index, entries,  list_average, std_dev, median_number, low_num, high_num, high_num_index, low_num_index
-    # ?????????????????????? Is it better coding practice to split the statistics into different functions? too many returns = not future proof????????????????????
-
 def change_to_numeric(data):
     num_list = []
     for value in data:
@@ -149,7 +82,7 @@ def statistics_stddev(data):
 def statistics_median(data):
     #sort the list to get the median, largest, and smallest numbers
     my_list_sorted = data[:] 
-    my_list_sorted.sort() # why is this function white? it is working...????????????????????????????????????????????
+    my_list_sorted.sort() 
     median_number = my_list_sorted[int(round((len(my_list_sorted)/2),0))]
     return median_number
 
